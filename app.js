ed345fc3560e2fdd978d8d44b256b17a112fa292
pr0fix <bgx152@myy.haaga-helia.fi>
2025-02-08 15:05:22 +0200

    fix broken code

--- app.js
+++ app.js
@@ -8,8 +8,6 @@ const PORT = process.env.PORT || 5000
 app.use(staticExpress('dist'))
 
 app.get('/health', (_req, res) => {
-  // eslint-disable-next-line no-constant-condition
-  if (true) throw 'error...  '
   res.send('ok')
 })
 
