65184e7b976a21f52daeb4a68be4860f9dfac5ef
pr0fix <bgx152@myy.haaga-helia.fi>
2025-02-08 15:01:41 +0200

    fix simulated broken version lint errors

--- app.js
+++ app.js
@@ -7,11 +7,11 @@ const PORT = process.env.PORT || 5000
 
 app.use(staticExpress('dist'))
 
-app.get("/health", (_req, res) => {
+app.get('/health', (_req, res) => {
   // eslint-disable-next-line no-constant-condition
-  if (true) throw "error...  ";
-  res.send("ok");
-});
+  if (true) throw 'error...  '
+  res.send('ok')
+})
 
 app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`)
