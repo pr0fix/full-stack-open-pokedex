4f7fb429190601932ee9c35a7af91b10ae389290
pr0fix <bgx152@myy.haaga-helia.fi>
2025-02-08 14:48:51 +0200

    add health check endpoint

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ const PORT = process.env.PORT || 5000
 
 app.use(staticExpress('dist'))
 
+app.get('/health', (_req, res) => {
+  res.send('ok')
+})
+
 app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`)
 })
